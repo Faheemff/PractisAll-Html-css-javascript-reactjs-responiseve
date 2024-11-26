8ac7bca9b374a1213add9703529ec05489de9af9
Faheemff <mfaheemweb@gmail.com>
2024-11-26 21:55:33 +0500

    add more files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Header from './Component/Header';
+import UseState from './Hooks/UseState';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <UseState />
     </div>
   );
 }
---
+++ src/Hooks/UseState.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
+// import Buttons from '../Component/Buttons'
+
+const UseState = () => {
+    let [a,b] = useState(90)
+  return (
+    <div>
+      <h1 className='pl-44 text-2xl'>{a}</h1>
+      <button onClick={()=>{b(a+1)}} type='button' className='px-7 py-2 rounded-lg bg-red-800 text-white ml-64 text-2xl'>Click</button>
+    </div>
+  )
+}
+
+export default UseState
