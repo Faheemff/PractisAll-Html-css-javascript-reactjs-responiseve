c78680ac3debee04d8bbac3da8ddf34e21c48796
Faheemff <mfaheemweb@gmail.com>
2024-11-26 09:33:03 +0500

    add more files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen rounded-3xl w-full bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1719937050445-098888c0625e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8")]'>
+    <div className=''>
       
     </div>
   );
