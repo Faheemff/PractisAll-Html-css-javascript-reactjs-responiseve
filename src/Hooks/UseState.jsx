087338cabcc69c32994e9bd1f01f4ee413b27084
Faheemff <mfaheemweb@gmail.com>
2024-12-15 20:11:01 +0500

    add

--- src/Hooks/UseState.jsx
+++ src/Hooks/UseState.jsx
@@ -9,7 +9,7 @@ const UseState = () => {
       <h1 className='pl-44 text-2xl'>{a}</h1>
       <button onClick={()=>{b(a+1)}} type='button' className='px-7 py-2 rounded-lg bg-red-800 text-white ml-64 text-2xl'>Click</button>
 
-      {/*Sedding data in other compnent*/}
+      {/*Sending data in other component*/}
 
       
     </div>
