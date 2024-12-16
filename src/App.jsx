61ba06ccbfc16a59d5ba5f4f41f61fc9dd298c91
Faheemff <mfaheemweb@gmail.com>
2024-12-16 20:05:10 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Header from './Component/Header';
-import UseState from './Hooks/UseState';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <UseState />
-    </div>
+    <>
+      <h1>app</h1>
+    </>
   );
 }
 
