a8daee2a12bd813647cb57dec081f32439742e8f
Faheemff <mfaheemweb@gmail.com>
2024-12-16 21:26:41 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Towaybinding from './Towaybinding'
 // import Form from './Form'
 
 const App = () => {
   return (
     <div>
       {/* <Form /> */}
+      <Towaybinding />
     </div>
   )
 }
--- src/Towaybinding.jsx
+++ src/Towaybinding.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 
 const Towaybinding = () => {
+    const [username, setUsername] = useState('')
   return (
     <div>
       <form>
-        <input className='m-3 text-xl text-black px-6 py-2 rounded-md' type="text" placeholder='Enter your name' />
+        <input
+        value={username}
+        className='m-3 text-xl text-black px-6 py-2 rounded-md' type="text" placeholder='Enter your name' />
         <button className='bg-gray-500 px-4 py-2 rounded-md'>Submit</button>
       </form>
     </div>
