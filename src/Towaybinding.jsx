1a038ed99fd7dbde081c774d17b78c25b6578f3c
Faheemff <mfaheemweb@gmail.com>
2024-12-16 21:36:58 +0500

    add

--- src/Towaybinding.jsx
+++ src/Towaybinding.jsx
@@ -2,12 +2,22 @@
 import React, { useState } from 'react'
 
 const Towaybinding = () => {
-    const [username, setUsername] = useState('')
+    const [username, setUsername] = useState('');
+    const submit = (e)=> {
+        e.preventDefault();
+        console.log(username);
+        setUsername('');
+    }
   return (
     <div>
-      <form>
+      <form onSubmit={(e)=> {
+        submit(e);
+      }}>
         <input
         value={username}
+        onChange={(e)=> {
+            setUsername(e.target.value);
+        }}
         className='m-3 text-xl text-black px-6 py-2 rounded-md' type="text" placeholder='Enter your name' />
         <button className='bg-gray-500 px-4 py-2 rounded-md'>Submit</button>
       </form>
